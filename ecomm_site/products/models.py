0df2472310b2a5daca95e850346c389879f73630
Dave Caffrey <dave.caffrey71@gmail.com>
2020-04-30 16:47:35 +0100

    Basic product model/view/url mapping

--- ecomm_site/products/models.py
+++ ecomm_site/products/models.py
@@ -1,17 +1,24 @@
+import django
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 
 
 class Product(models.Model):
+    """
+    A single Product
+    """
     name = models.CharField(max_length=256)
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    date_added = models.DateTimeField(default=timezone.now())
+    date_added = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
-    def buy_product(self):
-        pass
+
 
 
 
--- ecomm_site/products/views.py
+++ ecomm_site/products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Product
+from django.views.generic import ListView
 
+
+class ProductListView(ListView):
+    model = Product
+    # if pagination required
+    # paginate_by = 100
 
