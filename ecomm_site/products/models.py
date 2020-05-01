9c848413fbf2efdaaa21cfa9b935f2f0d841db82
Dave Caffrey <dave.caffrey71@gmail.com>
2020-05-01 15:09:52 +0100

    product detail view works

--- ecomm_site/products/models.py
+++ ecomm_site/products/models.py
@@ -18,6 +18,14 @@ def upload_image_path(instance, filename):
     return "products/{new_filename}/{final_filename}".format(new_filename=new_filename, final_filename=final_filename)
 
 
+# class ProductManager(models.manager):
+#     def get_by_id(self, id):
+#         qs = self.get_queryset().filter(id=id)
+#         if qs.count() == 1:
+#             return qs.first()
+#         return None
+
+
 class Product(models.Model):
     """
      A single Product
@@ -27,6 +35,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     date_added = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to=upload_image_path, blank=True, null=True)
+
+    # objects = ProductManager()
 
     def __str__(self):
         return self.name
--- ecomm_site/products/views.py
+++ ecomm_site/products/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from django.views.generic import ListView, DetailView
@@ -11,9 +12,23 @@ class ProductListView(ListView):
 
 class ProductDetailView(DetailView):
     queryset = Product.objects.all()
+    template_name = "products/product_detail.html"
 
-    def get_object(self):
-        obj = super().get_object()
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args,**kwargs)
+        print(context)
+        return context
+
+    # def get_object(self, *args, **kwargs):
+    #     request = self.request
+    #     pk = self.kwargs.get('pk')
+    #     instance = Product.objects.get_by_id(pk)
+    #     if instance is None:
+    #         raise Http404("Product does not exist Dave")
+    #     return instance
+
+    # def get_object(self):
+        # obj = super().get_object()
         # Record the last accessed date
-        obj.save()
-        return obj
+        # obj.save()
+        # return obj
